18943e8f2245315fe486b7e134f6cbc85fca4db3
Joy Kiega <jwkiega@gmail.com>
2025-08-01 00:18:23 +0800

    new file adjusted

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,17 @@
 from bookshelf.models import Author, Book, Library, Librarian
 
+# Author book query
+author = Author.objects.get("specific author")
+Book.objects.filter(author)
 
-Author.objects.all()
+# Library books query
+library = Library.objects.get(name="library_name")
+library.books.all()
+["Library.objects.get(name=library_name)" , "books.all()"]
 
-Library.objects.all()
+# Librarian query
+librarian = Librarian.objects.get(name = "specific librarian")
+librarian.library
+
 
 
