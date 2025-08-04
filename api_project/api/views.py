7095b4b2034dedb40da12beaf29f6b5945fa041c
Joy Kiega <jwkiega@gmail.com>
2025-08-04 15:52:11 +0800

    new files added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# api/views.py
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
