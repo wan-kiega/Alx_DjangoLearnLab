e6e329281d2b349e79eafbb2ebc9439fbc3f19ed
Joy Kiega <jwkiega@gmail.com>
2025-08-11 15:18:13 +0300

    new files added

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%nekgu65segq)=n))z%t2t5en6)2o46g(oj8+=a(i0(r30_u6'
+SECRET_KEY = 'django-insecure-4+3m!4qy856!z2utabek1ng!$^cnojcef67@a2gheo$t*qh#-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
