815cb808c1de38b75d1355c20f37fe3c682e2674
Joy Kiega <jwkiega@gmail.com>
2025-08-11 17:33:33 +0300

    new files added

---
+++ django_blog/django_blog/blog/admin.py
@@ -0,0 +1,9 @@
+# blog/admin.py
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'published_date']
+    list_filter = ['published_date', 'author']
+    search_fields = ['title', 'content']
